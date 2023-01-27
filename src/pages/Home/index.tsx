c1d7d4ed1a850500392701ae36f10763f16803af
Enzo Costa <enzo_costa7@hotmail.com>
2023-01-26 22:57:05 -0300

    add loading in product details and home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,17 @@ import './styles.scss'
 import { Product } from '../../types/Product'
 import api from '../../services/api'
 import { Card } from '../../components/Card'
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Home() {
   const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function getProducts() {
       const products = await api.get('/products/')
       setProducts(products.data)
+      setLoading(false)
     }
 
     getProducts()
@@ -19,19 +22,29 @@ function Home() {
 
   console.log(products)
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <div className="product-card">
-          {products.map((item, index) => (
-            <Card 
-              item={item} 
-              key={index}  
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+    <>
+          {loading && (
+            <div className='loading'>
+              <ClipLoader color="#36d7b7" size={82} />
+            </div>
+          )}
+          {!loading && (
+            <div className="App">
+              <Header />
+              <div className="container">
+                <div className="product-card">
+                  {products.map((item, index) => (
+                    <Card 
+                      item={item} 
+                      key={index}  
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+    </>
+    
   )
 }
 
--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -6,15 +6,18 @@ import { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 import { Product } from '../../types/Product';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [item, setItem] = useState<Product | null>({} as Product);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function getProduct() {
       let res = await api.get(`/products/${id}`)
       setItem(res.data)
+      setLoading(false)
     }
 
     getProduct()
@@ -22,9 +25,17 @@ const ProductDetails = () => {
   }, [])
 
   return (
-    <h1>
-      {item?.title}
-    </h1>
+    <>
+      {loading && (
+        <div className='loading'>
+          <ClipLoader color="#36d7b7" size={82} />
+        </div>
+      )}    
+      {!loading && (
+        <h1>{item?.title}</h1>
+      )}
+    </>
+    
   )
 }
 
